fd99bb28c45f22bc07cc8e3da03347ef4857d60e
Matt Tilley <mtilley255@gmail.com>
2017-08-19 11:50:38 -0500

    Coolness
    Just doing a commit because I've been tinkering around and...forgot.

--- primary.js
+++ primary.js
@@ -4,18 +4,48 @@ var c = document.getElementById("BOX03")
 var d = document.getElementById("BOX04")
 var e = document.getElementById("BOX05")
 
-var cycler = [a, b, c, d]
+var cycler = [a, b, c, d];
+
+
+$.each(cycler, function(i, item){
+		
+		setTimeout(function(){
+			$(item)	.fadeIn(500);
+		}, i * 500);
+		
+		trigger();
+		
+	})
+
+
+function trigger()
+{
+	$("#BOX05")	.delay(2000)
+				.fadeTo(2000, 1);
+}
+
+/*$.each(cycler, function(i, item)
+{
+	setTimeout(function()
+	{
+		$(item).fadeIn(2000).fadeOut(2000);
+		
+		
+	} i * 1000);
+	
+	
+});*/
 
 
 
-$.each(intro_1, function(i, item){
+/*$.each(intro_1, function(i, item){
 		
 		setTimeout(function(){
 			$(item)	.delay(2000).fadeIn(1500)
 					.fadeOut(1500).delay(2000);
 		}, i * 3000);
 		
-	})
+	})*/
 
 
 
