52935a03393191f57e16e3be1a51f5901019cd0d
Matt Tilley <mtilley255@gmail.com>
2017-08-19 11:14:40 -0500

    Declared more JS variables.

--- primary.js
+++ primary.js
@@ -1,5 +1,8 @@
 var a = document.getElementById("BOX01")
-
+var b = document.getElementById("BOX02")
+var c = document.getElementById("BOX02")
+var d = document.getElementById("BOX02")
+var e = document.getElementById("BOX02")
 
 $(document).ready(function()
 {
