a3a953c32b35303b3d9686dd3aa68c38b2a3c699
Matt Tilley <mtilley255@gmail.com>
2017-08-19 11:26:52 -0500

    Grabbed code from my other project.

--- primary.js
+++ primary.js
@@ -6,7 +6,20 @@ var e = document.getElementById("BOX05")
 
 var cycler = [a, b, c, d]
 
-$(document).ready(function()
+
+
+$.each(intro_1, function(i, item){
+		
+		setTimeout(function(){
+			$(item)	.delay(2000).fadeIn(1500)
+					.fadeOut(1500).delay(2000);
+		}, i * 3000);
+		
+	})
+
+
+
+/*$(document).ready(function()
 {
 	for (i = 0; i < cycler.length; i++)
 	{
@@ -14,4 +27,4 @@ $(document).ready(function()
 	}
 	
 	
-});
+});*/
