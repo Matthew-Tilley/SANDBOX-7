21106fece28f43e2598744e35a116ad6ea82786e
Matt Tilley <mtilley255@gmail.com>
2017-08-19 11:19:59 -0500

    Cool...getting closer.

--- primary.js
+++ primary.js
@@ -1,10 +1,17 @@
 var a = document.getElementById("BOX01")
 var b = document.getElementById("BOX02")
-var c = document.getElementById("BOX02")
-var d = document.getElementById("BOX02")
-var e = document.getElementById("BOX02")
+var c = document.getElementById("BOX03")
+var d = document.getElementById("BOX04")
+var e = document.getElementById("BOX05")
+
+var cycler = [a, b, c, d]
 
 $(document).ready(function()
 {
-	$(a).fadeTo(2000, .5);
+	for (i = 0; i < cycler.length; i++)
+	{
+		$(cycler[i]).fadeTo(1000, .25);
+	}
+	
+	
 });
